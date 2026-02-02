c4cc8d6965403ad83eb1f50fc39f75440b9045e6
Kundan@88 <kundansingh20296@gmail.com>
2026-02-02 10:17:16 +0530

    changes

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -7,7 +7,6 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.DriverFactory;
 import com.qa.opencart.pages.AccountsPage;
@@ -15,6 +14,8 @@ import com.qa.opencart.pages.LoginPage;
 import com.qa.opencart.pages.ProductInfoPage;
 import com.qa.opencart.pages.RegisterPage;
 import com.qa.opencart.pages.ResultsPage;
+
+import io.qameta.allure.Step;
 
 public class BaseTest {
 	
@@ -28,30 +29,33 @@ public class BaseTest {
 	protected ProductInfoPage productInfoPage;
 	protected RegisterPage registerPage;
 	
-	protected SoftAssert softAssert;
+	//protected SoftAssert softAssert;
 	
-	@Parameters({"browser"})
+	@Step("Setup with browser : {0} and init the propeties")
+	@Parameters({"browser", "browserversion", "testname"})
 	@BeforeTest
-	public void setup(@Optional("chrome") String browserName) {
+	public void setup(@Optional("chrome") String browserName, String browserVersion, String testName) {
 		df = new DriverFactory();
 		prop = df.initProp();		
 
 			//check if browser param is coming from testng.xml
 			if(browserName!=null) {
 				prop.setProperty("browser", browserName);
+				prop.setProperty("browserversion", browserVersion);
+				prop.setProperty("testname", testName);
+
 			}
 		
 		
 		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
-		softAssert = new SoftAssert();
+		//softAssert = new SoftAssert();
 	}
 	
-	
+	@Step("close the browser")
 	@AfterTest
 	public void tearDown() {
 		driver.quit();
 	}
 	
-
 }
